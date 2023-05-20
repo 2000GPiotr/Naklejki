f3e7d41d711a2e057d92c2d31a2315c3fe34a393
Piotrek <gpiotrek2000@gmail.com>
2023-05-20 14:13:03 +0200

    Add RoleController

---
+++ Naklejki/Controllers/RoleController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.DataTransferModels.Roles;
+using Services.Interfaces;
+
+namespace API.Controllers
+{
+    [Route("/Role")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRolesService _roleService;
+        public RoleController(IRolesService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<RoleDto>>> GetAllRoles()
+        {
+            var roles = await _roleService.GetAllRoles();
+            return Ok(roles);
+        }
+    }
+}
