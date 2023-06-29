3d12d6ec1db2c31f93e7d217a8988449c6247130
Piotrek <gpiotrek2000@gmail.com>
2023-06-29 13:27:30 +0200

    Interfejsy uzywane przez documentHeaderService

---
+++ Repository/Interfaces/IDocumentTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IDocumentTypeRepository
+    {
+        Task<List<DocumentType>> GetAllDocumentTypes();
+        Task<DocumentType> GetDocumentTypeBySymbol(string symbol);
+    }
+}
---
+++ Repository/Interfaces/IItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IItemRepository
+    {
+        Task AddItem(Item item);
+        Task UpdateItem(Item item);
+        Task DeleteItem(Item item);
+        Task<List<Item>> GetItemByDocumentId(int id);
+        Task AddItemRange(List<Item> items);
+        Task UpdateItemRange(List<Item> items);
+        Task DeleteItemRange(List<Item> items);
+    }
+}
