783976e91355f3f2566f443414983d6db103e2d5
Piotrek <gpiotrek2000@gmail.com>
2023-08-23 14:58:46 +0200

    Czesc testow TODO

---
+++ TestsRepositories/ItemRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsRepositories
+{
+    public class ItemRepositoryTest
+    {
+        DbContextOptions<LabelDbContext> CreateOptions(string dbName)
+        {
+            return new DbContextOptionsBuilder<LabelDbContext>()
+                .UseInMemoryDatabase(dbName)
+                .Options;
+        }
+
+        //TODO
+    }
+}
